cede9a7b49c2b1e79fb3507f7ed49485d59de88a
Chandradas-Dessai <37234889+Chandradas-Dessai@users.noreply.github.com>
2020-02-24 21:19:01 +0530

    new files
    new files

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
-const express = require('express');
-	const path = require('path');
-	 
-	const app = express();
-	 
-	
-	app.use(express.static(__dirname + '/dist/eslabs-prj'));
-	 
-	app.get('/*', function(req,res) {
-	 
-	res.sendFile(path.join(__dirname+'/dist/eslabs-prj/index.html'));
-	});
-	 
-	
-	app.listen(process.env.PORT || 8080);
+const path = require("path");
+const express = require("express");
+const app = express();
+app.use(express.static(__dirname + '/dist/eslabs-prj'));
+app.get('/*', function(req,res){
+res.sendFile(path.join(__dirname, '/dist/eslabs-prj', 'index.html'))
+});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
