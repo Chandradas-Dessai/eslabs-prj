d615707c485dccacfd832292d7fd7dea6dac443f
Chandradas-Dessai <37234889+Chandradas-Dessai@users.noreply.github.com>
2020-02-24 21:23:17 +0530

    node to nodemon

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 app.use(express.static(__dirname + '/dist/eslabs-prj'));
 app.get('/*', function(req,res){
-res.sendFile(path.join(__dirname, '/dist/eslabs-prj', 'index.html'))
+res.sendFile(path.join(__dirname, 'dist/eslabs-prj', 'index.html'))
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
